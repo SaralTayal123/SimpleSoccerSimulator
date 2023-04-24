c714ed94e4e6cfbbe10b203e6f50e571e39e3088
Saral Tayal <saraltayal@sarals-air.wifi.local.cmu.edu>
2023-04-24 17:49:05 -0400

    updated 2player env to have team.right start more right

--- sim.py
+++ sim.py
@@ -10,7 +10,7 @@ env = Enviornment()
 # env.init_random_game(2)
 env.init_2player_game()
 
-agent2 = Agent(team=Team.RIGHT)
+agent2 = AgentNaive(team=Team.RIGHT)
 agent1 = MacroAgent(team=Team.LEFT)
 
 while(env.gameRunning):
